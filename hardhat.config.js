f784835db603c0385d623daecb2d9d7f38db4bc9
Abdul Maajid <amaajid0@gmail.com>
2021-11-17 18:10:28 +0500

    Working completed by questbook

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,11 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 const LOCAL_URL = "http://127.0.0.1:8545/"; 
-const PRIVATE_KEY = "";
+const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 module.exports = {
   solidity: "0.7.3",
-  network: {
+  networks: {
     local: {
       url: LOCAL_URL,
       accounts : [PRIVATE_KEY]
